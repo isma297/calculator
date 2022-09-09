14beb3b5b563d4c2ecad53272849744b204f6543
isma297 <ismael.vega297@gmail.com>
2022-09-09 17:04:58 -0300

    Add functions: add, subtract, multiply and divide All functions take multiple arguments.

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(numbers){
+    return numbers.reduce((a,b)=>{return a+b})
+}
+function subtract(numbers){
+    return numbers.reduce((a,b)=>{return a-b})
+    }
+function multiply(numbers){
+    return numbers.reduce((a,b)=>{return a*b})
+    }
+function divide(numbers){
+    return numbers.reduce((a,b)=>{return a/b})
+    }
+
+function operate(sign,...numbers){
+    console.log(numbers)
+    return switch(sign){
+        case '+': add(numbers);break;
+        case '-': subtract(numbers);break;
+        case '*': multiply(numbers);break;
+        case '/': divide(numbers);break;
+    }
+}
+console.log(operate('+',5,5,4,6))
