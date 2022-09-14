f9f3e10101153b5d2b3050f223ffc477f241dc62
isma297 <ismael.vega297@gmail.com>
2022-09-14 19:10:52 -0300

    Limit the size of numbers input to  9 digit

--- script.js
+++ script.js
@@ -2,49 +2,47 @@ const numberButtons = document.querySelectorAll('.number');
 const displayValue = document.querySelector('#result');
 const operator = document.querySelectorAll('.operator')
 const ac = document.querySelector('#ac');
-const equal=document.querySelector('.equal');
-equal.addEventListener('click', ()=>{equals()});
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', () => { equals() });
 
 ac.addEventListener('click', () => { allClear() })
-function allClear () {displayValue.textContent = '';numArr=[];num2=[];i=0;numString=''};
+function allClear() { displayValue.textContent = ''; numArr = []; num2 = []; i = 0; numString = '' };
 
 numberButtons.forEach((button) => { button.addEventListener('click', () => { writeNumber(button.id) }); })
 
 operator.forEach((button) => { button.addEventListener('click', () => { writeOperator(button.id) }); })
-let numArr=[];
+let numArr = [];
 let i;
-let numString='';
+let numString = '';
 function writeNumber(a) {
-  console.log(a);
-    displayValue.textContent += a;
-    numString+=a;
-    i??=0;
-    console.log(i)
-    numArr[i]=numString;
-    
-}
-function writeOperator(a){
-  if(i==2){equals()};
+  if(numString.length<9){
   displayValue.textContent += a;
-  numArr[1]=a;
-  i=2;
-  numString='';
+  numString += a;
+  i ??= 0;
+  numArr[i] = numString;}
 }
-num2=[];
+function writeOperator(a) {
+  if (i == 2) { equals() };
+  displayValue.textContent += a;
+  numArr[1] = a;
+  i = 2;
+  numString = '';
+}
+num2 = [];
 function equals(a) {
   console.log(numArr);
-  for(let i=0;i<numArr.length;i++){
-    num2[i]=parseInt(numArr[i]);
-    if(isNaN(num2[i])){
-      num2[i]=numArr[i];
+  for (let i = 0; i < numArr.length; i++) {
+    num2[i] = parseInt(numArr[i]);
+    if (isNaN(num2[i])) {
+      num2[i] = numArr[i];
     }
-}
-let result=operate(numArr[1],numArr[0],numArr[2]);
+  }
+  let result = operate(numArr[1], numArr[0], numArr[2]);
   console.log(`equals = ${result}`)
   console.log(num2);
-displayValue.textContent+=`\n =${result}`;
-allClear();
-writeNumber(result);
+  displayValue.textContent += `\n =${result}`;
+  allClear();
+  writeNumber(result);
 }
 
 function add(numbers) {
@@ -57,7 +55,7 @@ function multiply(numbers) {
   return numbers.reduce((a, b) => { return a * b })
 }
 function divide(numbers) {
-  return numbers.reduce((a, b) => { return  b==0? displayValue.textContent='Boom!': (a / b) })
+  return numbers.reduce((a, b) => { return b == 0 ? displayValue.textContent = 'Boom!' : (a / b) })
 }
 
 function operate(sign, ...numbers) {
